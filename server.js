ca14825bf36022f7a894d949e6a76d914fac72b0
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-07-09 09:55:39 +0530

    cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ dotenv.config()
 const saltRounds = 10; // Number of salt rounds for bcrypt
 
 const corsOptions = {
-  origin: "https://recruitment-solution-frontend-ggknsxzvb-vaishali054.vercel.app/", // frontend URI (ReactJS)
+  origin: "https://recruitment-solution-frontend-rdls6f599-vaishali054.vercel.app", // frontend URI (ReactJS)
 }
 app.use(cors(corsOptions));
 app.use(express.json())
